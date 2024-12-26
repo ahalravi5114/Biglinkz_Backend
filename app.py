45e2cd5d8085b0700d8d0e351db1daecc8528381
ahalravi5114 <ahalya5114@gmail.com>
2024-12-26 17:41:13 +0530

    added

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import cloudinary.api
 from threading import Thread
 
 app = Flask(__name__)  
-CORS(app)
+CORS(app, origins="*")
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
